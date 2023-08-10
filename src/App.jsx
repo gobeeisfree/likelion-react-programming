4c8a32b79335dd1335cf8259b807140b3e5c7879
gobeeisfree <gobeeisfree@gmail.com>
2023-08-10 09:12:46 +0900

    export, export default 차이점 해설
    - utils 모듈
    - getRandom, getRandomMinMax
    - currency, currencyKR
    - numberWithComma

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-import RootLayout from './layout/RootLayout';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Products from './pages/Products';
+import RootLayout from "./layout/RootLayout";
+import * as Utils from "./utils";
 
-// {/* 홈 페이지 */}
-// {/* 소개 페이지 */}
-// {/* 제품 목록 페이지 */}
-// {/* 의뢰 페이지 */}
+console.log(Utils);
+
+// import Home from './pages/Home';
+// import About from './pages/About';
+
+// {/* 홈 페이지 : index.html */}
+// {/* 소개 페이지 : about.html */}
+// {/* 제품 목록 페이지 : products.html */}
+// {/* 의뢰 페이지 : contact.html */}
 
 function App() {
   return (
     <div className="App">
       <RootLayout>
-        <Home />
-        {/* <About />
-        <Contact />
-        <Products /> */}
+        {/* <Home /> */}
+        <output style={{ display: "block" }}>
+          {/* function composition */}
+          {/* {currencyKR(numberWithComma(getRandom(1920999)))} */}
+        </output>
+        {/* <output>{getRandomMinMax()}</output> */}
+        <output>
+          {Utils.currencyKR(Utils.numberWithComma(99870 + 19000 + 9400 + 870))}
+        </output>
       </RootLayout>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// React 함수 컴포넌트의 요건
-// - 함수 이름은 첫글자가 대문자
-// - JSX를 반환!
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
-
-// JSX가 하는 일은 React 요소 생성 → 마크업 생성
-// JSX → 컴파일러(변환기) → React 요소 → ReactDOM → 실제 DOM 요소 생성 → 구조 작성

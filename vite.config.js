82835ca41784d04092b8f29c78100962e3308ae4
gobeeisfree <gobeeisfree@gmail.com>
2023-08-10 11:34:43 +0900

    Vite 절대 경로 구성
    - resolve.alias
    - rollup alias plugin
    - Node.js path module

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// import { resolve } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     devSourcemap: true,
+  },
+  resolve: {
+    // alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
+    alias: {
+      // '@': resolve(__dirname, './src'),
+      '@': '/src',
+    },
   },
 });

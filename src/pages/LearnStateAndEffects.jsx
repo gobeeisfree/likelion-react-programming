0c618cf609d9b6680735217bd42a4c5e71148967
gobeeisfree <gobeeisfree@gmail.com>
2023-08-16 17:07:15 +0900

    Tailwind CSS : SUIT 폰트 확장

--- src/pages/LearnStateAndEffects.jsx
+++ src/pages/LearnStateAndEffects.jsx
@@ -54,7 +54,9 @@ function LearnStateAndEffects() {
 
   return (
     <div className="m-10 flex flex-col items-start gap-2">
-      <h2 className={`text-2xl text-indigo-600 ${fontWeight} uppercase`}>
+      <h2
+        className={`font-suit text-2xl text-indigo-600 ${fontWeight} uppercase`}
+      >
         상태 및 이펙트 학습하기
       </h2>
 

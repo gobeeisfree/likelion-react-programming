0b98b7a990f682ae785e385626c95cb89aafe5e9
gobeeisfree <gobeeisfree@gmail.com>
2023-08-18 19:31:51 +0900

    useDocumentTitle 훅

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,8 @@
+import useDocumentTitle from '@/hooks/useDocumentTitle';
+
 function Contact() {
+  useDocumentTitle('의뢰');
+
   return (
     <div>
       <h2 className="text-emerald-500">Contact</h2>

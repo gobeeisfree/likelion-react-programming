5991c8633d78acc4b3697ec8171461032d3dc6a0
gobeeisfree <gobeeisfree@gmail.com>
2023-08-28 09:52:46 +0900

    상품 등록 페이지 폼 디자인

--- src/pages/ProductNew.jsx
+++ src/pages/ProductNew.jsx
@@ -1,110 +1,157 @@
 import pb from '@/api/pocketbase';
-import { useRef } from 'react';
-import { Helmet } from 'react-helmet-async';
+import { useRef, useState } from 'react';
 import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 function ProductNew() {
+  const navigate = useNavigate();
+
+  const formRef = useRef(null);
   const titleRef = useRef(null);
   const colorRef = useRef(null);
   const priceRef = useRef(null);
   const photoRef = useRef(null);
-  const uploadPhotoRef = useRef(null);
 
-  const handleRegisterProduct = async (e) => {
+  const handleRegister = async (e) => {
     e.preventDefault();
 
-    const formData = new FormData();
-    formData.append('title', titleRef.current.value);
-    formData.append('color', colorRef.current.value);
-    formData.append('price', Number(priceRef.current.value));
-    formData.append('photo', photoRef.current.files[0]);
+    const titleValue = titleRef.current.value;
+    const colorValue = colorRef.current.value;
+    const priceValue = Number(priceRef.current.value);
+    const photoValue = photoRef.current.files;
 
-    try {
-      await pb.collection('products').create(formData);
-      toast.success('상품 등록에 성공했습니다!', {
-        position: 'top-center',
+    if (!titleValue && !colorValue && !priceValue) {
+      toast('이름, 색상, 가격 정보 입력이 필요합니다.', {
+        icon: '🚨',
         ariaProps: {
           role: 'status',
           'aria-live': 'polite',
         },
       });
+
+      return;
+    }
+
+    const formData = new FormData();
+
+    formData.append('title', titleValue);
+    formData.append('color', colorValue);
+    formData.append('price', priceValue);
+    if (photoValue) {
+      formData.append('photo', photoValue[0]);
+    }
+
+    try {
+      await pb.collection('products').create(formData);
+      navigate('/products');
     } catch (error) {
       console.error(error);
     }
   };
 
-  const handleDisplayUploadPhoto = (e) => {
-    const photoFile = e.target.files[0];
-    const photoUrl = URL.createObjectURL(photoFile);
-    uploadPhotoRef.current.setAttribute('src', photoUrl);
+  const handleReset = () => {
+    titleRef.current.value = '';
+    colorRef.current.value = '';
+    priceRef.current.value = '';
+    photoRef.current.value = '';
+    setFileImages([]);
+  };
+
+  const [fileImages, setFileImages] = useState([]);
+
+  const handleUpload = (e) => {
+    const { files } = e.target;
+    const fileImages = Array.from(files).map((file) => ({
+      image: URL.createObjectURL(file),
+      label: file.name,
+    }));
+    setFileImages(fileImages);
   };
 
   return (
-    <>
-      <Helmet>
-        <title>Register Product - ReactBird</title>
-      </Helmet>
-      <div className="flex flex-col gap-6">
-        <h2 className="text-center text-3xl">상품 등록</h2>
-        <form encType="multipart/form-data" onSubmit={handleRegisterProduct}>
-          <div>
-            <label htmlFor="title">이름</label>
+    <div className="container mx-auto max-w-lg">
+      <h2 className="my-5 text-center text-2xl font-medium text-blue-950">
+        상품 등록
+      </h2>
+      <form
+        encType="multipart/form-data"
+        ref={formRef}
+        onSubmit={handleRegister}
+        onReset={handleReset}
+        className="flex flex-col items-center gap-2"
+      >
+        <div className="flex w-full flex-col gap-2">
+          <label htmlFor="title">이름</label>
+          <input
+            ref={titleRef}
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Slim Fit Ribbed Sleeveless"
+            className="w-full rounded-md border border-slate-300 px-4 py-1.5 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2"
+          />
+        </div>
+        <div className="flex w-full flex-col gap-2">
+          <label htmlFor="color">색상</label>
+          <input
+            type="text"
+            ref={colorRef}
+            name="color"
+            id="color"
+            placeholder="Black"
+            className="w-full rounded-md border border-slate-300 px-4 py-1.5 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2"
+          />
+        </div>
+        <div className="flex w-full flex-col gap-2">
+          <label htmlFor="price">가격</label>
+          <input
+            type="number"
+            ref={priceRef}
+            name="price"
+            id="price"
+            placeholder="49000"
+            step="100"
+            className="w-full rounded-md border border-slate-300 px-4 py-1.5 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2"
+          />
+        </div>
+        <div className="flex w-full flex-col gap-2">
+          <label htmlFor="photo">사진</label>
+          <div className="relative">
             <input
-              ref={titleRef}
-              type="text"
-              name="title"
-              id="title"
-              placeholder="상품 이름"
-            />
-          </div>
-          <div>
-            <label htmlFor="color">색상</label>
-            <input
-              ref={colorRef}
-              type="text"
-              name="color"
-              id="color"
-              placeholder="색상 이름(예: Black)"
-            />
-          </div>
-          <div>
-            <label htmlFor="price">가격</label>
-            <input
-              ref={priceRef}
-              type="text"
-              name="price"
-              id="price"
-              placeholder="가격 (예: 98000)"
-            />
-          </div>
-          <div className="relative my-4 flex flex-col gap-2">
-            <label htmlFor="photo">사진</label>
-            <input
-              ref={photoRef}
-              onChange={handleDisplayUploadPhoto}
-              className="absolute h-full w-full cursor-pointer opacity-0"
               type="file"
+              accept="*.jpg,*.png,*.jpeg,*.webp,*.avif"
+              ref={photoRef}
               name="photo"
               id="photo"
-              accept="*.jpg,*.png,*.webp,*.avif"
-              multiple
+              // multiple
+              onChange={handleUpload}
+              className="absolute z-10 h-full w-full cursor-pointer opacity-0"
             />
-            <div className="h-36 bg-slate-200/80 p-2">
-              <img
-                ref={uploadPhotoRef}
-                className="h-full border border-slate-400/50"
-                src="https://placehold.co/84x124?text=PHOTO"
-                alt=""
-              />
+            <div className="flex h-36 w-full gap-2 overflow-x-auto bg-slate-100 p-2">
+              {fileImages.map((file) => {
+                return (
+                  <img key={file.label} src={file.image} alt={file.label} />
+                );
+              })}
             </div>
           </div>
-          <div className="flex gap-2">
-            <button type="submit">등록</button>
-            <button type="reset">취소</button>
-          </div>
-        </form>
-      </div>
-    </>
+        </div>
+        <div className="mb-6 mt-4 flex w-full justify-center gap-2">
+          <button
+            type="submit"
+            className="rounded-full border-2 border-slate-300 px-3.5 py-1 hover:border-slate-400"
+          >
+            등록
+          </button>
+          <button
+            type="reset"
+            className="rounded-full border-2 border-slate-200 bg-slate-200 px-3.5 py-1 hover:border-slate-300 hover:bg-slate-300"
+          >
+            취소
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
